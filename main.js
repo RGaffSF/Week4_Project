59ca37b5d4570d4e2f6dab8ab9d4bf57e4513fe8
Branden <bsmceachern@gmail.com>
2021-08-09 16:21:12 -0500

    this further develops our API search functionality. search works just need to finish display info of movie data

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 document.getElementById('movieLookup').addEventListener('click', function (event) {
     event.preventDefault();
+
     let movieTitle = document.getElementById('movieTitle').value
+    let responseObj = {};
+
     fetch('http://www.omdbapi.com/?apikey=a6df6408&t=' + movieTitle)
         .then((response) => {
             if (response.ok) {
-                console.log(response.json());
+                responseObj = response.json();
+                return responseObj;
+            } else {
+                window.alert('Network Error!')
+            }
+        })
+        .then(movieData => {
+            if (movieData.Response === "True") {
+                console.log(movieData)
             } else {
                 window.alert('Movie not found!')
             }
         })
 })
+
+
+const displayResults = () => {
+
+}
+
+
+
+
