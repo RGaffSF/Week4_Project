73928ab6331469b1d7aad0ee7c9feaa9ecce57db
Randall <randall.gaffner.t1w9@statefarm.com>
2021-08-10 10:49:52 -0500

    making a second fetch

--- main.js
+++ main.js
@@ -1,10 +1,49 @@
-document.getElementById('movieLookup').addEventListener('click', function (event) {
+// document.getElementById('movieLookup').addEventListener('click', function (event) {
+//     event.preventDefault();
+
+//     let movieTitle = document.getElementById('movieTitle').value
+//     let responseObj = {};
+
+//     fetch('http://www.omdbapi.com/?apikey=a6df6408&t=' + movieTitle)
+//         .then((response) => {
+//             if (response.ok) {
+//                 responseObj = response.json();
+//                 return responseObj;
+//             } else {
+//                 window.alert('Network Error!')
+//             }
+//         })
+//         .then(movieData => {
+//             if (movieData.Response === "True") {
+//                 // console.log(movieData)
+//                 return displayResults(movieData)
+//             } else {
+//                 window.alert('Movie not found!')
+//             }
+//         })
+//         .catch(() => {
+//             window.alert('Network Error! Check Your Internet')
+//         })
+// })
+
+
+// const displayResults = (movieData) => {
+//     document.getElementById('title').innerText = "Title: " + movieData.Title
+//     document.getElementById('genre').innerText = "Genre: " + movieData.Genre
+//     document.getElementById('rated').innerText = "Rated: " + movieData.Rated
+//     document.getElementById('runtime').innerText = "Runtime: " + movieData.Runtime
+//     document.getElementById('metascore').innerText = "Metascore: " + movieData.Metascore
+// }
+
+
+document.getElementById('Top10Lookup').addEventListener('click', function (event) {
     event.preventDefault();
 
-    let movieTitle = document.getElementById('movieTitle').value
-    let responseObj = {};
+    let movieGenre = document.getElementById('PickedGenre').value
 
-    fetch('http://www.omdbapi.com/?apikey=a6df6408&t=' + movieTitle)
+    //fetch('http://www.omdbapi.com/?apikey=a6df6408&g=' + movieGenre)
+
+   fetch ('https://www.imdb.com/search/title/?genres=' + movieGenre + '&sort=user_rating')
         .then((response) => {
             if (response.ok) {
                 responseObj = response.json();
@@ -16,22 +55,13 @@ document.getElementById('movieLookup').addEventListener('click', function (event
         .then(movieData => {
             if (movieData.Response === "True") {
                 console.log(movieData)
-                return displayResults(movieData)
+                //return displayResults(movieData)
             } else {
                 window.alert('Movie not found!')
             }
         })
+        .catch(() => {
+            window.alert('Network Error! Check Your Internet')
+        })
 })
-
-
-const displayResults = (movieData) => {
-    document.getElementById('title').innerText = "Title: " + movieData.Title
-    document.getElementById('genre').innerText = "Genre: " + movieData.Genre
-    document.getElementById('rated').innerText = "Rated: " + movieData.Rated
-    document.getElementById('runtime').innerText = "Runtime: " + movieData.Runtime
-    document.getElementById('metascore').innerText = "Metascore: " + movieData.Metascore
-}
-
-
-
 
