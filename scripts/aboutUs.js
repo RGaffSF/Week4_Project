c8180805566bc051a7511719533a0860edf923c7
Randall <randall.gaffner.t1w9@statefarm.com>
2021-08-12 09:38:48 -0500

    added spans with + signs

--- scripts/aboutUs.js
+++ scripts/aboutUs.js
@@ -1,14 +1,24 @@
-var coll = document.getElementsByClassName("collapsible2");
-var i;
+let coll = document.getElementsByClassName("collapsible2");
 
-for (i = 0; i < coll.length; i++) {
+for (var i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
     this.classList.toggle("reveal");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
+      return coll.getElementsByTagName("span") = " - "
     } else {
       content.style.display = "block";
     }
   });
 }
+
+
+// function minussign () {
+
+// let test = document.getElementsByClassName("collapsible2").innertext
+// console.log(test);
+// let test = document.getElementsByClassName("collapsible2").span.innertext = " +  Django Unchained"
+// console.log(test);
+// }
+
