493c8b8c1fb35670166635e2fa79585a1f8165ee
Randall <randall.gaffner.t1w9@statefarm.com>
2021-08-12 10:28:25 -0500

    added pluses and minus for collapsible

--- scripts/aboutUs.js
+++ scripts/aboutUs.js
@@ -3,22 +3,17 @@ let coll = document.getElementsByClassName("collapsible2");
 for (var i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
     this.classList.toggle("reveal");
-    var content = this.nextElementSibling;
+    const inner = this.innerHTML;
+    const content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
-      return coll.getElementsByTagName("span") = " - "
+      this.innerHTML = " + " + inner.substring(3,inner.length)
     } else {
       content.style.display = "block";
+      this.innerHTML = " - " + inner.substring(3,inner.length)
+         
     }
   });
 }
 
-
-// function minussign () {
-
-// let test = document.getElementsByClassName("collapsible2").innertext
-// console.log(test);
-// let test = document.getElementsByClassName("collapsible2").span.innertext = " +  Django Unchained"
-// console.log(test);
-// }
 
