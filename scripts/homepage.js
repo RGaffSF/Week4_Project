47f3bb9a211cf183a0ed572bbe48eb84ea301dc0
Randall <randall.gaffner.t1w9@statefarm.com>
2021-08-12 11:59:12 -0500

    added banana pictures

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,11 +1,13 @@
+let bananapic = document.getElementById("bpic");
+
 document.getElementById('movieLookup').addEventListener('click', function (event) {
     event.preventDefault();
 
     let movieTitle = document.getElementById('movieTitle').value
 
 
-   fetchMovieData(movieTitle)
-   getRandomImage()
+    fetchMovieData(movieTitle)
+    getRandomImage()
 })
 
 
@@ -18,13 +20,26 @@ const displayResults = (movieData) => {
     document.getElementById('Released').innerText = "Released: " + movieData.Released
     document.getElementById('BoxOffice').innerText = "Box Office: " + movieData.BoxOffice
     document.getElementById('Plot').innerText = "Plot: " + movieData.Plot
+    if (movieData.Metascore > 90) {
+        bananapic.innerHTML = ('<img src=' + '"assets/P1_Banana.jpg">'+ "5 out of 5 Bananas")
+    } else if
+        (movieData.Metascore > 80 && movieData.Metascore < 89) {
+        bananapic.innerHTML = ('<img src=' + '"assets/P2_Banana.jpg">'+ "4 out of 5 Bananas")
+    } else if
+        (movieData.Metascore > 70 && movieData.Metascore < 79) {
+        bananapic.innerHTML = ('<img src=' + '"assets/P3_Banana.jpg">'+ "3 out of 5 Bananas")
+    } else if
+        (movieData.Metascore > 60 && movieData.Metascore < 69) {
+        bananapic.innerHTML = ('<img src=' + '"assets/P4_Banana.jpg">'+ "2 out of 5 Bananas")
+    } else
+        bananapic.innerHTML = ('<img src=' + '"assets/P4_Banana.jpg">'+ "1 out of 5 Bananas")
 }
 
 
-let ImageArray = ['Img1.jfif','Img2.png','Img3.jpg'];
+let ImageArray = ['Img1.jfif', 'Img2.png', 'Img3.jpg'];
 
 function getRandomImage() {
-    const num = Math.floor( Math.random() * 3);
+    const num = Math.floor(Math.random() * 3);
     const img = ImageArray[num];
     document.getElementById("randImage").innerHTML = ('<img src="' + 'assets/random_pics/' + img + '" width="250px">')
 }
